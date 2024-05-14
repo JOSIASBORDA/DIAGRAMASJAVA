b588d2443c61359c114714b5fa88c10f0c377012
JOSIASBORDA <1430227@senati.pe>
2024-05-14 08:15:23 -0500

    Add files via upload

---
+++ PAPELERIADIAGRAMAS/src/main/java/com/mycompany/papeleriadiagramas/PAPELERIADIAGRAMAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.papeleriadiagramas;
+
+/**
+ *
+ * @author Josias Choccata
+ */
+public class PAPELERIADIAGRAMAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
